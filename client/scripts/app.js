561879d008a673527a33de3a4011a32d4cf0a8ce
tps-80 <thomaspsorensen@gmail.com>
2015-10-19 16:59:35 -0700

    Implement roomName, username clicking, message submit

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,110 @@
-// YOUR CODE HERE:
+var app = {
+  server: 'https://api.parse.com/1/classes/chatterbox',
+  rooms: {},
+  friends: [],
+  currentRoom: "default",
+  init: function() {
+    var context = this;
+    $('.submit').on('click', function(){
+      context.handleSubmit();
+    });
+
+    // setInterval(function() {
+      context.fetch();
+    // }, 2000);
+  },
+
+  handleSubmit: function() {
+    var context = this;
+    var username = _.last(window.location.search.split('='));
+    var roomName = $('#roomSelect').val();
+    var text = $('.msgText').val();
+    var message = {
+      username: username,
+      roomname: roomName,
+      text: text
+    }
+    context.send(message);
+  },
+
+  send: function(message) {
+    var context = this;
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+        console.log(data);
+        context.addMessage(message);
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  },
+
+  fetch: function(){
+    var context = this;
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'GET',
+      data: '-createdAt',
+      contentType: 'application/json',
+      success: function (data) {
+        //iterate over data.results
+        for (var i = 0; i < data.results.length; i++) {
+
+          if (data.results[i].text) {
+            var tmp = data.results[i].roomname;
+            if (tmp){
+              var lowerTmp = tmp.toLowerCase();
+              if (!context.rooms[lowerTmp]) {
+                context.rooms[lowerTmp] = tmp;
+                context.addRoom(tmp);
+              }
+            }
+            context.addMessage(data.results[i]);
+          }
+
+        }
+        console.log(data);
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to get messages');
+      }
+    });
+  },
+
+  clearMessages: function(){
+    $('#chats').children().remove();
+  },
+
+  addMessage: function(message){
+    var context = this;
+    var msgDiv = $('<div id="chat"></div>');
+    var text = $('<p class="text">' + message.text + '</p>');
+    var user = $('<a class="username">User: ' + message.username + '</a>');
+    user.on('click', function() {
+      context.addFriend(message.username);
+    });
+    msgDiv.append(user);
+    msgDiv.append(text); 
+    $('#chats').prepend(msgDiv);
+  },
+
+  addRoom: function(roomName) {
+    $('#roomSelect').append('<option value="' + roomName.toLowerCase() + '">' + roomName + '</option>');
+  },
+
+  addFriend: function(username) {
+    var context = this;
+    if (context.friends.indexOf(username) === -1) {
+      context.friends.push(username);
+    }
+  }
+
+};
+
+$('document').ready(function() {app.init();})
